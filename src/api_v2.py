e86d6967210d845d4997d474cd7b03b43e3533c7
Deveshjhunjhunnwala <deveshjw228@gmail.com>
2026-05-19 22:29:29 +0530

    added staticfiles fir testing

--- src/api_v2.py
+++ src/api_v2.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
+from pathlib import Path
 
 app = FastAPI()
 
@@ -10,6 +12,18 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+frontend_dist = Path(__file__).resolve().parent.parent / "frontend" / "dist"
+
+print("Frontend path:", frontend_dist)
+print("Exists:", frontend_dist.exists())
+
+if (frontend_dist / "assets").exists():
+    app.mount(
+        "/assets",
+        StaticFiles(directory=frontend_dist / "assets"),
+        name="assets",
+    )
 
 @app.get("/")
 def root():
